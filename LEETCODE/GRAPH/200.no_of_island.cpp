ecd221cacf09735a95d9f78535fdcba78661149a
karan2310-hub <karan.backend@gmail.com>
2026-07-11 20:24:49 +0530

    graph 200 corrected

--- LEETCODE/GRAPH/200.no_of_island.cpp
+++ LEETCODE/GRAPH/200.no_of_island.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
-void bfs(vector<vector<char>>&grid,vector<vector<bool>>&visited,int i,int j){
-  
+void bfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int row, int col){
+ int rows=grid.size();
+ int cols=grid[0].size();
+ queue<pair<int,int>>q;
+ q.push({row,col});
+ while(!q.empty()){
+    auto curr=q.front();//for neighbours
+    q.pop();
+    int r=curr.first;
+    int c=curr.second;
+    int dr[]={-1,1,0,0};
+    int dc[]={0,0,-1,1};
+    for(int i=0; i<4;i++){
+        int nr=r+dr[i];
+        int nc=c+dc[i];
+        //pehla neigh mila nr nc
+        if(nr>=0 and nr<rows and nc>=0 and nc<cols
+        and visited[nr][nc]!=true and grid[nr][nc]=='1'){
+            visited[nr][nc]=true;
+            bfs(grid,visited,nr,nc);
+        }
+    }
+
+ }
+ return;
+
 }
     int numIslands(vector<vector<char>>& grid) {
         int rows=grid.size();
-        int no_island=0;
         int cols=grid[0].size();
-        // queue<pair<int,int>>q;
-        vector<vector<bool>>visited(rows,vector<bool>(cols,false));
-        for(int i=0;i<rows;i++  ){
-            for(int j=0;j<cols;j++){
-                if(visited[i][j]=false){
-                    no_island++;
-                   bfs(grid,visited,i,j);
+        int no_of_island=0;
+        vector<vector<bool>>visited(rows,vector<bool>(cols,false));      
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(grid[i][j]=='1' and visited[i][j]!=true){
+                    no_of_island++;
+                    bfs(grid,visited,i,j);
                 }
             }
         }
-        return no_island;
+        return no_of_island;
     }
 };
